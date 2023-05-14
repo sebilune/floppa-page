8e8e4702069a24ab671584b30562e67b4d8269d6
Sebastian Ramirez <sebastianramord@gmail.com>
2023-05-14 02:50:31 -0500

    Improved style of Container N.2. Added double the cards for a more complete feeling. Been also playing around with audio functions, might implement more of that.

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function playSound() {
-    var audio = document.getElementById("myAudio");
+    var audio = document.getElementById("roarSound");
+    audio.play();
+}
+
+function playSoundOne() {
+    var audio = document.getElementById("dreamSound");
     audio.play();
 }
